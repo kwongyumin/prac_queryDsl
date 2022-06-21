5820ff013bebdb4dc8b8078a3a35706e3228a611
kwongyumin <rbals7398@naver.com>
2022-06-21 22:44:23 +0900

    QueryDsl 결과 조회 및 정렬 테스트코드 작성

--- src/test/java/com/practice/querydsl/QueryDslBasicTest.java
+++ src/test/java/com/practice/querydsl/QueryDslBasicTest.java
@@ -5,6 +5,7 @@ import com.practice.querydsl.model.Member;
 import com.practice.querydsl.model.QMember;
 import com.practice.querydsl.model.Team;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 
@@ -13,6 +14,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
+import java.util.List;
 
 import static com.practice.querydsl.model.QMember.member;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -109,4 +112,62 @@ public class QueryDslBasicTest {
         assertThat(findMember.getUsername()).isEqualTo("member1");
 
     }
+
+    @Test
+    public void resultFetch(){
+//        List<Member> fetch = queryFactory
+//                .selectFrom(member)
+//                .fetch(); // 리스트반환
+//
+//        Member fetchOne = queryFactory
+//                .selectFrom(QMember.member)
+//                .fetchOne(); // 단건 조회 (2건 이상이면 notUniqueException 발생 )
+//
+//        Member fetchFirst = queryFactory
+//                .selectFrom(QMember.member)
+//                .fetchFirst(); // 처음 한 건 조회
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults(); // QueryResults 페이징에서 사용
+        results.getTotal();
+        List<Member> content = results.getResults();
+
+
+        long total = queryFactory  //전체 갯수 만 조회
+                .selectFrom(member)
+                .fetchCount();
+    }
+
+    /*
+        회원 정렬 순서
+        1. 회원 나이 내림차순(desc)
+        2. 회원 이름 올림차순(asc)
+        2 에서 회원 이름 없을 시 , 마지막에 출력
+     */
+
+    @Test
+    public void sort(){
+        em.persist(new Member(null,100));
+        em.persist(new Member("member5",100));
+        em.persist(new Member("member6",100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(
+                        member.age.eq(100)
+                )
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+
+    }
+
+
+
 }

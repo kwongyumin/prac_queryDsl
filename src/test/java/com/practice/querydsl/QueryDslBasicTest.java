f7ecd6b25acdc7ce6461c9124eb206100e351704
kwongyumin <rbals7398@naver.com>
2022-06-22 22:29:49 +0900

    QueryDsl 페이징 및 집합 테스트코드 작성

--- src/test/java/com/practice/querydsl/QueryDslBasicTest.java
+++ src/test/java/com/practice/querydsl/QueryDslBasicTest.java
@@ -3,9 +3,11 @@ package com.practice.querydsl;
 
 import com.practice.querydsl.model.Member;
 import com.practice.querydsl.model.QMember;
+import com.practice.querydsl.model.QTeam;
 import com.practice.querydsl.model.Team;
 
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 
@@ -18,6 +20,7 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 import static com.practice.querydsl.model.QMember.member;
+import static com.practice.querydsl.model.QTeam.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -168,6 +171,81 @@ public class QueryDslBasicTest {
 
     }
 
+    @Test // 페이징 처리
+    public void paging1(){
+        List<Member> results = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1) // 시작 인덱스
+                .limit(2) // 호출할 갯수
+                .fetch();
+
+        assertThat(results.size()).isEqualTo(2);
+
+    }
+
+    @Test
+    public void paging2(){
+        QueryResults<Member> queryResults = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetchResults();
+
+        assertThat(queryResults.getTotal()).isEqualTo(4); // 전체 갯수
+        assertThat(queryResults.getLimit()).isEqualTo(2);
+        assertThat(queryResults.getOffset()).isEqualTo(1);
+        assertThat(queryResults.getResults().size()).isEqualTo(2);
+
+
+    }
+
+    @Test
+    public void aggregation(){
+        List<Tuple> result = queryFactory
+                .select( // 결과값이 튜플로 나온다.
+                        member.count(),
+                        member.age.sum(),
+                        member.age.avg(),
+                        member.age.max(),
+                        member.age.min()
+                )
+                .from(member)
+                .fetch();
+
+        Tuple tuple = result.get(0);
+        assertThat(tuple.get(member.count())).isEqualTo(4);
+        assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+        assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+        assertThat(tuple.get(member.age.max())).isEqualTo(40);
+        assertThat(tuple.get(member.age.min())).isEqualTo(10);
+
+
+    }
+    /*
+        팀의 이름과 각 팀의 평균연령을 구하자.
+
+     */
+
+    @Test
+    public void group() throws Exception{
+        List<Tuple> results = queryFactory
+                .select(team.name, member.age.avg())
+                .from(member)
+                .join(member.team, team) // 멤버의 팀과 팀을 조인
+                .groupBy(team.name) //팀의 이름으로 그룹핑
+                .fetch();
+
+        Tuple teamA = results.get(0);
+        Tuple teamB = results.get(1);
+
+        assertThat(teamA.get(team.name)).isEqualTo("teamA");
+        assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+
+        assertThat(teamB.get(team.name)).isEqualTo("teamB");
+        assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+    }
 
 
 }

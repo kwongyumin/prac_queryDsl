fd8d322323afaca16abe1645d5d5fa68dd3e13e9
kwongyumin <rbals7398@naver.com>
2022-06-30 22:25:16 +0900

    QueryDsl 상수,문자 더하기 테스트 코드 작성

--- src/test/java/com/practice/querydsl/QueryDslBasicTest.java
+++ src/test/java/com/practice/querydsl/QueryDslBasicTest.java
@@ -9,6 +9,7 @@ import com.practice.querydsl.model.Team;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
@@ -494,5 +495,34 @@ public class QueryDslBasicTest {
             System.out.println("s = " + s);
         }
     }
+
+    @Test
+    public void constant(){
+        List<Tuple> result = queryFactory
+                .select(member.username, Expressions.constant("A"))
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+
+    }
+    @Test
+    public void concat(){
+
+        //username_age
+        List<String> result = queryFactory
+                .select(member.username.concat("_").concat(member.age.stringValue()))
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+
+    }
+
 }
 

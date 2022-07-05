f0a713283d967b85fe891024fc3f278884a6fdc0
kwongyumin <rbals7398@naver.com>
2022-07-05 23:38:52 +0900

    QueryDsl 프로젝션 ::DTO 결과값 반환 테스트 코드 작성 2

---
+++ src/main/java/com/practice/querydsl/dto/UserDto.java
@@ -0,0 +1,18 @@
+package com.practice.querydsl.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class UserDto {
+
+    private String name;
+    private int age;
+
+
+    public UserDto(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+}

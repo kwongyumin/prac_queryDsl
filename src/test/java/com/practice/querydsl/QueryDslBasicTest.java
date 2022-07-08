072e5935e789c0309b9d6f9b72ab0cc083def8a0
kwongyumin <rbals7398@naver.com>
2022-07-08 23:11:36 +0900

    QueryDsl BooleanBuilder 사용 테스트코드 작성

--- src/test/java/com/practice/querydsl/QueryDslBasicTest.java
+++ src/test/java/com/practice/querydsl/QueryDslBasicTest.java
@@ -9,6 +9,7 @@ import com.practice.querydsl.model.QMember;
 import com.practice.querydsl.model.QTeam;
 import com.practice.querydsl.model.Team;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
@@ -653,6 +654,36 @@ public class QueryDslBasicTest {
     }
 
 
+    @Test
+    public void dynamicQuery_BooleanBuilder(){
+        String usernameParam = "member1";
+        Integer ageParam = 10;
+
+        List<Member> result = searchMember1(usernameParam,ageParam);
+
+        assert result != null;
+        assertThat(result.size()).isEqualTo(1);
+
+    }
+
+    private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+
+        BooleanBuilder builder = new BooleanBuilder(); // 생성자에 초기값을 넣을 수 있다.
+        if(usernameCond != null){
+            builder.and(member.username.eq(usernameCond));
+        }
+
+        if(ageCond != null){
+            builder.and(member.age.eq(ageCond));
+        }
+
+
+        return queryFactory
+                .selectFrom(member)
+                .where(builder) // 결과값이 나온 Builder를 넣기만하면된다.
+                .fetch();
+
+    }
 
 
 }
